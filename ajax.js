c55a34a7fa483628365d4e20c90da4c94959114f
Daniel Ang <dan.justin.ang@gmail.com>
2018-10-30 17:59:54 -0400

    completed steps 1 - 5

--- ajax.js
+++ ajax.js
@@ -1,5 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
+var root = document.body.querySelector('.root')
+var ping = document.body.querySelector('.ping')
 
-  /* Your code goes here */
+root.addEventListener("click", function(){
+  axios({
+  url: 'http://intro-ajax-api.herokuapp.com/',
+  method: 'get',
+  responseType: 'text',
+    });
+});
+
+ping.addEventListener("click", function(){
+  axios({
+  url: 'http://intro-ajax-api.herokuapp.com/pong',
+  method: 'get',
+  responseType: 'text',
+}).then(function(response){
+  console.log(response.data);
+  var something = document.createElement('p')
+  something.innerHTML = response.data
+  document.body.querySelector('#step3456').appendChild(something);
+}).catch(function(error){
+  console.log(error);
+  var errorError = document.createElement('p')
+  errorError.innerHTML = "WHY IS THIS ERRORING";
+  document.body.querySelector('#step3456').appendChild(errorError);
+})
+});
+
+
 
 });
